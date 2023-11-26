2a4ee349580b3129c7d6a5132f186e5483d8ee16
Nuray Korganci <nrysnmz@gmail.com>
2023-11-26 14:11:44 -0600

    m

--- src/main/java/com/example/mockservice/impl/GBPPostingControllerImpl.java
+++ src/main/java/com/example/mockservice/impl/GBPPostingControllerImpl.java
@@ -1,66 +1,64 @@
-package com.example.mockservice.impl;
-
-import com.example.mockservice.gbpController.GBPPostingController;
-import com.example.mockservice.model.Error;
-import com.example.mockservice.model.GBPPostingRequestBody;
-import com.example.mockservice.model.GBPPostingResponseBody;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@Slf4j
-public class GBPPostingControllerImpl implements GBPPostingController {
-
-    private String SUCCESS = "SUCCESS";
-    private String BAD_REQUEST = "BAD_REQUEST";
-    private String TIMEOUT = "TIMEOUT";
-
-    private List<String> supportedResponseTypes = List.of(SUCCESS, BAD_REQUEST);
-    private String requestedResponseType = SUCCESS;
-
-    @Override
-    public ResponseEntity<String> toogleResponse(String responseType) {
-        requestedResponseType = responseType.toUpperCase();
-        return ResponseEntity.status(HttpStatus.OK).body("GBPPosting " + responseType + " supported response types " + supportedResponseTypes);
-    }
-
-    @Override
-    public ResponseEntity<Object> requestGBPPosting(GBPPostingRequestBody postingRequestBody, HttpHeaders httpHeaders) throws InterruptedException {
-
-        if (StringUtils.equalsAnyIgnoreCase(requestedResponseType, TIMEOUT)) {
-            Thread.sleep(2000);
-            return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).build();
-        } else if (StringUtils.equalsAnyIgnoreCase(requestedResponseType, BAD_REQUEST)) {
-            var errorResponse = Error.builder().statusCode(400).errorDescription("failed").build();
-            return ResponseEntity.badRequest().body(errorResponse);
-        } else {
-            return ResponseEntity.ok(mockGBPPostingResponseBodyCompleted());
-        }
-
-        GBPPostingResponseBody mockGBPPostingResponseBody() {
-
-            GBPPostingResponseBody gbpPostingResponseBodyBuilder =
-                    GBPPostingResponseBody.builder().transactionIdentifier("3443").transactionStatusCode("TRANSACTION_RETRYABLE").build();
-
-            return GBPPostingResponseBody.builder();
-
-
-        }
-
-        GBPPostingResponseBody mockGBPPostingResponseBodyCompleted(){
-
-            GBPPostingResponseBody gbpPostingResponseBodyBuilder =GBPPostingResponseBody.builder().transactionIdentifier("3443").transactionStatusCode("TRANSACTION_RETRYABLE").build();
-            return GBPPostingResponseBody.builder();
-        }
-
-    }
-
-}
+//package com.example.mockservice.impl;
+//
+//import com.example.mockservice.gbpController.GBPPostingController;
+//import com.example.mockservice.model.Error;
+//import com.example.mockservice.model.GBPPostingRequestBody;
+//import com.example.mockservice.model.GBPPostingResponseBody;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@Slf4j
+//public class GBPPostingControllerImpl implements GBPPostingController {
+//
+//    private String SUCCESS = "SUCCESS";
+//    private String BAD_REQUEST = "BAD_REQUEST";
+//    private String TIMEOUT = "TIMEOUT";
+//
+//    private List<String> supportedResponseTypes = List.of(SUCCESS, BAD_REQUEST);
+//    private String requestedResponseType = SUCCESS;
+//
+//    @Override
+//    public ResponseEntity<String> toogleResponse(String responseType) {
+//        requestedResponseType = responseType.toUpperCase();
+//        return ResponseEntity.status(HttpStatus.OK).body("GBPPosting " + responseType + " supported response types " + supportedResponseTypes);
+//    }
+//
+//    @Override
+//    public ResponseEntity<Object> requestGBPPosting(GBPPostingRequestBody postingRequestBody, HttpHeaders httpHeaders) throws InterruptedException {
+//
+//        if (StringUtils.equalsAnyIgnoreCase(requestedResponseType, TIMEOUT)) {
+//            Thread.sleep(2000);
+//            return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).build();
+//        } else if (StringUtils.equalsAnyIgnoreCase(requestedResponseType, BAD_REQUEST)) {
+//            var errorResponse = Error.builder().statusCode(400).errorDescription("failed").build();
+//            return ResponseEntity.badRequest().body(errorResponse);
+//        } else {
+//            return ResponseEntity.ok(mockGBPPostingResponseBodyCompleted());
+//        }
+//
+//        GBPPostingResponseBody mockGBPPostingResponseBody() {
+//
+//            GBPPostingResponseBody gbpPostingResponseBodyBuilder =
+//
+//            return GBPPostingResponseBody.builder();
+//
+//
+//        }
+//
+//        GBPPostingResponseBody mockGBPPostingResponseBodyCompleted(){
+//
+//            return GBPPostingResponseBody.builder();
+//        }
+//
+//    }
+//
+//}
---
+++ src/main/java/com/example/mockservice/learning/JSON.java
@@ -0,0 +1,63 @@
+//package com.example.mockservice.learning;
+//
+//public class JSON {
+//
+//    {
+//        "postingBatchTransactionIdentifier": "3bb7a002-f189-4714-9ecc-e879169c7a8b",
+//            "financialTransactions": [
+//        {
+//            "postingInstructionTransactionIdentifier": "3bb7a002-f189-4714-9ecc-e879169c7a8b",
+//                "account": {
+//            "productIdentifier": "090",
+//                    "accountNumber": "00000000XX00000XXX00XXX",
+//                    "last4AccountNumber": "0998",
+//                    "bankIdentifier": "0000000802"
+//        },
+//            "amount": {
+//            "transactionAmount": 1,
+//                    "currencyCode": "USD"
+//        },
+//            "transactionTypeCode": "000012",
+//                "transactionReferenceNumber": "7563821",
+//                "clientReferenceNumber": "D0127PAYMENT",
+//                "transactionDescription": {
+//            "longTransactionDescription": "PAYMENT TO LOAN ACCT#0998"
+//        },
+//            "transactionDetails": {
+//            "transactionRequestTypeName": "GENERIC"
+//        },
+//            "transactionSettlementDate": "2023-02-01",
+//                "generalLedgerAccountNumber": 2071010023,
+//                "transactionPostTimestamp": "2023-02-01T17:05:38.270681Z",
+//                "financialTransactionIdentifier": "53743030-bd94-4adf-b342-52b047fe20b2"
+//        },
+//        {
+//            "postingInstructionTransactionIdentifier": "another-transaction-id",
+//                "account": {
+//            "productIdentifier": "091",
+//                    "accountNumber": "00000000XX00000XXX00YYY",
+//                    "last4AccountNumber": "0777",
+//                    "bankIdentifier": "0000000803"
+//        },
+//            "amount": {
+//            "transactionAmount": 2,
+//                    "currencyCode": "EUR"
+//        },
+//            "transactionTypeCode": "000013",
+//                "transactionReferenceNumber": "9876543",
+//                "clientReferenceNumber": "D0128PAYMENT",
+//                "transactionDescription": {
+//            "longTransactionDescription": "ANOTHER PAYMENT"
+//        },
+//            "transactionDetails": {
+//            "transactionRequestTypeName": "SPECIAL"
+//        },
+//            "transactionSettlementDate": "2023-02-02",
+//                "generalLedgerAccountNumber": 2071010024,
+//                "transactionPostTimestamp": "2023-02-02T18:15:45.123456Z",
+//                "financialTransactionIdentifier": "98765432-aaaa-bbbb-cccc-ddddeeeeffff"
+//        }
+//  ],
+//        "postingOperationStatusName": "POSTED"
+//    }
+//}
