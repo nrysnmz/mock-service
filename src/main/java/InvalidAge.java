75764380527702910a9cb0bad3d5b1b1d3d22010
Nuray Korganci <nrysnmz@gmail.com>
2023-12-03 15:06:48 -0600

    M

---
+++ src/main/java/InvalidAge.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class InvalidAge {
+
+
+        public static void main(String[] args) {
+            Scanner read = new Scanner(System.in);
+            String name = read.nextLine();
+            int age = read.nextInt();
+            Student student = new Student();
+            student.name = name;
+
+            //set the age via Setter
+            student.setAge(age);
+
+            System.out.println("Name: " + student.name);
+            System.out.println("Age: " + student.getAge());
+        }
+    }
+
+    class Student {
+
+        public String name;
+        private int age;
+
+        public int getAge() {
+            //complete Getter
+            return age;
+
+        }
+        public void setAge(int age) {
+            //complete Setter
+            if(age<0){
+                age=0;
+                System.out.println("Invalid age");
+            }
+            this.age=age;
+
+
+        }
+}
