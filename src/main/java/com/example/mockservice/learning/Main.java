4888fe734561e5a31d531284ee23054c66c30e90
Nuray Korganci <nrysnmz@gmail.com>
2023-12-04 20:33:00 -0600

    m

--- src/main/java/com/example/mockservice/learning/Main.java
+++ src/main/java/com/example/mockservice/learning/Main.java
@@ -1,71 +1,33 @@
 package com.example.mockservice.learning;
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
+
         Scanner read = new Scanner(System.in);
-        String firstName = read.nextLine();
-        String secondName = read.nextLine();
-        int age = read.nextInt();
-        int roomNumber = read.nextInt();
-        Customer customer = new Customer();
-        //set customer's data to object here
-        customer.setName(firstName);
-        customer.setSecondName(secondName);
-        customer.setAge(age);
-        customer.setRoomNumber(roomNumber);
+        int a = read.nextInt();
 
-        customer.saveCustomerInfo();
-    }
-}
-
-class Customer {
-    //add all necessary attributes here
-    private String firstName;
-    private String secondName;
-    private int age;
-    private int roomNumber;
-
-    public void setName(String firstName) {
-        this.firstName = firstName;
-
+        Pupil pupil = new Pupil();
+        pupil.setAge(a);
     }
 
-    public String getName() {
-        return firstName;
-    }
+    static class Pupil{
+        private int age;
 
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
+        //complete setter method
+        public void  setAge(int a){
+            this.age=a;
+            if(age>6){
 
-    public String getSecondName() {
-        return secondName;
-    }
+                System.out.println("Welcome");
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+            }else{
+                System.out.println("Sorry");
+            }
 
-    public int getAge() {
-        return age;
-    }
+        }
 
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public int getRoomNumber() {
-        return roomNumber;
-    }
-
-
-    public void saveCustomerInfo() {
-        System.out.println("First name: " + firstName);
-        System.out.println("Second name: " + secondName);
-        System.out.println("Age: " + age);
-        System.out.println("Room number: " + roomNumber);
+        public int getAge(){
+            return age;
+        }
     }
 }
