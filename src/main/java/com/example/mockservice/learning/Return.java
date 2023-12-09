632fc775bbe03a273e05f38f94b525f26a11afd9
Nuray Korganci <nrysnmz@gmail.com>
2023-12-09 11:31:20 -0600

    m

--- src/main/java/com/example/mockservice/learning/Return.java
+++ src/main/java/com/example/mockservice/learning/Return.java
@@ -1,11 +1,26 @@
 package com.example.mockservice.learning;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Return {
 
     public static void main(String[] args) {
 
 
 
+        List a = new ArrayList();
+        a.add(3);
+        a.add(3);
+        a.add(3);
+        a.add(3);
+
+        a.size();
+
+        int [] b ={4};
+        int c = b.length;
 
     }
+
 }
