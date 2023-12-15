139b7bf1baef27f27ac0764f9edffbc3692bedee
Nuray Korganci <nrysnmz@gmail.com>
2023-12-15 13:22:44 -0600

    m

---
+++ src/main/java/com/example/mockservice/learning/Prim.java
@@ -0,0 +1,19 @@
+package com.example.mockservice.learning;
+
+import org.hibernate.event.spi.SaveOrUpdateEvent;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpHeaders;
+import java.util.UUID;
+
+
+public class Prim {
+
+    public static void main(String[] args) {
+
+        String name = UUID.randomUUID().toString().replace("-","");
+
+    }
+
+    }
+
